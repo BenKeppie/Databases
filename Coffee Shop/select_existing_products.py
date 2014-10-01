4a5b3626d5d170e865b63f9139d43028701f192a
Ben Keppie <31291@longroad.ac.uk>
2014-10-01 11:06:21 +0100

    databse work

--- Coffee Shop/select_existing_products.py
+++ Coffee Shop/select_existing_products.py
@@ -7,7 +7,23 @@ def select_all_products():
         products=cursor.fetchall()
         return products
 
+def select_product(id):
+    with sqlite3.connect("coffee_shop.db") as db:
+        cursor=db.cursor()
+        cursor.execute("select * from Product where ProductID=?",(id,))
+        product=cursor.fetchone()
+        return product
+
+def select_product_name_price(id):
+    with sqlite3.connect("coffee_shop.db") as db:
+        cursor=db.cursor()
+        cursor.execute("select Name,Price from Product where ProductID=?",(id,))
+        product=cursor.fetchone()
+        return product
+
+
 if __name__=="__main__":
     products=select_all_products()
     print(products)
-#halfway through task 8
+    product=select_product(1)
+    print(product)
